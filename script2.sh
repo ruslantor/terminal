9bfb71091f7ac584b593f10d2c59cf0d4cf4e772
ruslantor <noixz22@gmail.com>
2020-09-23 22:28:45 +0300

    добавил срипт

---
+++ script2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# i = интернет есть
+# i = интернета нет
+i=1
+while true; do
+ping -c1 8.8.8.8
+result=$?
+echo "$result" "$i"
+if [[ "$result" -eq 0 && "$i" -eq 0 ]]
+then 
+echo "$(date +%d-%m-%y) - интернет появился в $(date +%H:%M)" >> journal.txt
+echo "$(date +%d-%m-%y) - интернет повился в $(date +%H:%M)"
+i=1
+elif [[ "result" -ne 0 && "$i" -eq 1 ]];
+then 
+echo "$(date +%d-%m-%y) - инетрнет пропал в $(date +%H:%M)" >> journal.txt
+echo "$(date +%d-%m-%y) - интернет пропал в $(date =%H:%M)"
+i=0
+fi
+sleep 5
+done
